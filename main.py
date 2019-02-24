e6cbf939d0106fa355b938ab857aeb55fbd5a515
Maria C Novoa Garcia <maria.novoa1@upr.edu>
2019-02-23 20:33:38 -0400

    Still Initial Commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+import handlers.convoHandler
 
 app = Flask(__name__)
 
@@ -14,13 +15,21 @@ app.route('/Sheeple/conversations/', methods=['GET', 'POST', 'PUT', 'DELETE' ])
     def getAllConvos():
         if request.method == 'POST':
             print("REQUEST: ", request.json)
-            return ConvoHandler().insertConvoJson(request.json)
+            return convoHandler().insertConvoJson(request.json)
         elif request.method == 'GET':
             print("REQUEST: ", request.json)
-            return ConvoHandler().getConvoJson(request.json)
+            return convoHandler().getConvoJson(request.json)
         elif request.method == 'POST':
             print("REQUEST: ", request.json)
-            return ConvoHandler().putConvoJson(request.json)
+            return convoHandler().putConvoJson(request.json)
+        else:
+            if not request.args:
+            return convoHandler().getAllConvos()
+        else:
+            return convoHandler().searchConvos(request.args)
+
+
+
 #@app.route('/Sheeple/contacts/')
 
 if __name__ == '__main__':
