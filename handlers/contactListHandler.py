c6ab73c3726d72c02cc9b61e1937c78ac8c3d3fa
luism013 <luis.rivera93@upr.edu>
2019-02-25 14:17:41 -0800

    started CL

---
+++ handlers/contactListHandler.py
@@ -0,0 +1,52 @@
+from flask import jsonify
+from daos.contactList import contactListDAO
+
+
+class contactListHandler:
+
+    def build_contactList_dict(self, row):
+        result = {}
+        result['contact_List_id'] = row[0]
+        result['contacts'] = row[1]
+        result['contact_amount'] = row[2]
+        result['owner_id'] = row[3]
+        return result
+
+    def getAllContactLists(self):
+        dao = contactListDAO()
+        result = dao.getAllContactLists()
+        mapped_result = []
+        for r in result:
+            mapped_result.append(self.build_user_dict(r))
+
+        return jsonify(ContactList=mapped_result)
+
+    def getContactListByID(self, id):
+        dao = contactListDAO()
+        result = dao.getContactListByID(id)
+        if result is None:
+            return jsonify(Error="NOT FOUND"), 404
+        else:
+            mapped_result = self.build_contactList_dict(result)
+
+        return jsonify(ContactList=mapped_result)
+
+    def searchContactLists(self, args):
+        dao = contactListDAO()
+        param1 = args.get('contacts')
+        param2 = args.get('contact_amount')
+        param3 = args.get('user_id')
+
+        if param1:
+            result = dao.get(param1)
+        elif param2:
+                result = dao.getPeopleByLastName(param2)
+        elif param3:
+            result = dao.getPeopleByGender(param3)
+        else:
+            return jsonify(Error="NOT FOUND"), 404
+        mapped_result = []
+        for r in result:
+            mapped_result.append(self.build_contactList_dict(r))
+
+        return jsonify(Person=mapped_result)
