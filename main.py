725a8b2c370d82774a5092d81833a62dbf59ee08
luism013 <luis.rivera93@upr.edu>
2019-02-24 16:28:36 -0800

    Merge branch 'daoCreator'
    # Conflicts:
    #	main.py
    
    solved

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from handlers.convoHandler import convoHandler
+from handlers.personHandler import personHandler
+
 
 app = Flask(__name__)
 
@@ -27,9 +29,31 @@ def getAllConvos():
     handler = convoHandler()
     return handler.getAllConvos()
 
-#@app.route('/Sheeple/conversations/<int=id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+# @app.route('/Sheeple/conversations/<int=id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 # @app.route('/Sheeple/contacts/')
+
+
+@app.route('/Sheeple/person', methods=['GET', 'POST', 'PUT', "DELETE"])
+def gettAllPeople():
+    if request.method == 'GET':
+        if request.args:
+            return personHandler().searchPeople(request.args)
+        else:
+            handler = personHandler()
+            return handler.getAllPeople()
+    elif request.method == 'POST':
+        return personHandler().postPerson()
+    elif request.method == 'PUT':
+        return personHandler().updatePerson()
+    else:
+        return personHandler().deletePerson()
+
+
+@app.route('/Sheeple/person/<int:person_id>', methods=['GET'])
+def getPersonById(person_id):
+    handler = personHandler()
+    return handler.getPersonByID(person_id)
 
 if __name__ == '__main__':
     app.run()
