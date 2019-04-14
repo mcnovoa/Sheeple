f4ff566ccfbe96351c1d7e647ad31f7853489153
luism013 <luis.rivera93@upr.edu>
2019-04-14 09:47:59 -0700

    resolved conflicts

--- sheepledb/dbconfig.py
+++ sheepledb/dbconfig.py
@@ -1,9 +1,9 @@
 pg_config = {
-#    'user' : 'marianovoa1',
-#    'passwd' : 'minix',
-#    'dbname' : 'sheepledb'
-
-    'user' : 'jonathan',
-    'passwd' : 'jonathan',
-    'dbname'  : 'sheeple'
+   'user' : 'marianovoa1',
+   'passwd' : 'minix',
+   'dbname' : 'sheepledb'
+#
+#     'user' : 'jonathan',
+#     'passwd' : 'jonathan',
+#     'dbname'  : 'sheeple'
 }
